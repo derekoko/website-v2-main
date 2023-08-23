c88703b5139f3073c4cbb2545297454f5e7ec7bd
Derek <dk.derek.koh@gmail.com>
2023-08-23 15:08:44 -0500

    creating seeqr12 website

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="description" content="SeeQR applicaiton landing page" />
+        <meta name="author" content="the SeeQR team" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import NavBar from "@/components/NavBar";
+import Main from "@/components/Main";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <NavBar />
+      <Main />
+      <Footer />
+    </>
+  );
+}
